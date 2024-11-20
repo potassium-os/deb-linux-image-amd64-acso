3cd501264818ceafbfb87e10729c895ad5ccbd54
Edwin Pers <pers.edwin@gmail.com>
2024-11-20 02:09:37 +0000

    userns keep-id might work

--- build-debs.sh
+++ build-debs.sh
@@ -49,7 +49,7 @@ export CONTAINER_IMAGE="ghcr.io/potassium-os/deb-linux-image-amd64-acso"
 podman run \
   --rm \
   -it \
-  --user builder \
+  --userns keep-id \
   -v "${SCRIPT_DIR}:/opt/potassium:rw" \
   -e "DEBIAN_VERSION=${DEBIAN_VERSION}" \
   -e "DEBUG=true" \
